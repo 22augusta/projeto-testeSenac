aca44d4e90812ad228699b05254c88dbaeb27141
22augusta <93688427+22augusta@users.noreply.github.com>
2021-11-23 19:28:46 -0300

    Alteração para rodar sem erros

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 const index = require('./routes/index');
-const series = require('./require/seriesRoute');
+const series = require('./routes/seriesRoutes');
 
-app.use((req, res, next)=> {
-    console.log('Nova requisicao realizada');
-
+app.use(function(req, res, next) {
+    res.header('Accss-Control-Allow-Origin','*')
+    res.header(
+        'Access-Control-Allow-Origin',
+        'Origin,X-Requested-With,Conent-Type,Accept'
+   
+    )
     next()
 });
 

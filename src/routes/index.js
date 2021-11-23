14991f317535a59bc208a4ecc76a9aa350b5f5df
22augusta <93688427+22augusta@users.noreply.github.com>
2021-11-23 19:30:10 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-//const express = require('express');//
+const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send({
+router.get('/', function(req, res) {
+    res.status(200).send({
         titulo:'Series do ArrowVerso para assistir quando estiver de bobeira',
         data: "19/11/2021"
     })

c9c54fb39408a3e2440bb0f6918102621b91f029
22augusta <93688427+22augusta@users.noreply.github.com>
2021-11-23 19:29:40 -0300

    Alteração para rodar 100%

--- src/routes/seriesRoutes.js
+++ src/routes/seriesRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const controller = require('../controller/seriesController')
 
-router.get("/", controller.getAll);
-router.get("/series", controller.getAll);
+router.get("/", controller.getAllSeries);
+
 
 module.exports = router;
